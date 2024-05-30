af5dcd7bc84b41717be38ed14dd4d2cec411bde0
jf7515@partner.kit.edu <jf7515@haicn1992.localdomain>
2024-05-30 22:04:38 +0200

    added pre-commit hook

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,16 +1,15 @@
 # tests/test_import.py
 
 import sys
+
 sys.path.append("../build")
+
 
 def test_import():
     try:
-        import pyGinkgo
+        import pyGinkgo  # noqa: F401
+
         assert True  # If import succeeds, the test passes
-        
-        
+
     except ImportError:
         assert False  # If import fails, the test fails
-        
-       
-
