28f847a346561f77a12f4ea1a9f8d17b57e5bda9
Gregor Olenik <gregor.olenik@web.de>
2024-06-13 14:34:29 +0200

    fixup basic array implementation

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,8 +1,7 @@
-# SPDX-License-Identifier: MIT
+# SPDX - License - Identifier : MIT
 #
-# SPDX-FileCopyrightText: 2024 pyGinkgo authors
+# SPDX - FileCopyrightText : 2024 pyGinkgo authors
 
-# tests/test_array.py
 
 import sys
 import pyGinkgo
@@ -11,7 +10,7 @@ sys.path.append("../build")
 
 
 def test_init_empty_array():
-    executor = pyGinkgo.Executor()
+    executor = pyGinkgo.ReferenceExecutor()
     size = 5
     arr = pyGinkgo.base.array(executor, size)
     assert arr.get_size() == size
