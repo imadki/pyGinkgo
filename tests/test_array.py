4f447bbd2f1c27c150649e17b0d529f212816af3
Gregor Olenik <gregor.olenik@web.de>
2024-06-19 11:04:13 +0200

    add further array tests

--- tests/test_array.py
+++ tests/test_array.py
@@ -5,6 +5,7 @@
 
 import sys
 import pyGinkgo
+import numpy as np
 
 sys.path.append("../build")
 
@@ -14,3 +15,43 @@ def test_init_empty_array():
     size = 5
     arr = pyGinkgo.base.array(executor, size)
     assert arr.get_size() == size
+
+
+def test_can_fill_array():
+    executor = pyGinkgo.ReferenceExecutor()
+    size = 5
+    arr = pyGinkgo.base.array(executor, size)
+    arr.fill(10)
+    assert arr.at(0) == 10
+
+
+def test_can_instantiate_array_from_python_list():
+    executor = pyGinkgo.ReferenceExecutor()
+    lst = [1, 2, 3, 4, 5]
+    arr = pyGinkgo.base.array(executor, lst)
+    assert arr.get_size() == len(lst)
+    assert arr.at(0) == 1
+    assert arr.at(1) == 2
+    assert arr.at(2) == 3
+    assert arr.at(3) == 4
+    assert arr.at(4) == 5
+
+
+def test_can_instantiate_array_from_numpy_array():
+    executor = pyGinkgo.ReferenceExecutor()
+    np_array = np.array([1, 2, 3, 4, 5])
+    arr = pyGinkgo.base.array(executor, np_array)
+    assert arr.get_size() == len(np_array)
+    assert arr.at(0) == 1
+    assert arr.at(1) == 2
+    assert arr.at(2) == 3
+    assert arr.at(3) == 4
+    assert arr.at(4) == 5
+
+
+def test_can_copy_construct_array():
+    executor = pyGinkgo.ReferenceExecutor()
+    np_array = np.array([1, 2, 3, 4, 5])
+    arr = pyGinkgo.base.array(executor, np_array)
+    arr_copy = pyGinkgo.base.array(executor, arr)
+    assert arr.get_size() == arr_copy.get_size()
