6c15211b81c1bc77905e4a89b68a5bb74d0497e2
Gregor Olenik <gregor.olenik@web.de>
2024-06-19 11:38:24 +0200

    add pybuffer and copy constructor

--- src/array.cpp
+++ src/array.cpp
@@ -2,6 +2,7 @@
 //
 // SPDX-FileCopyrightText: 2024 pyGinkgo authors
 
+#include "pybind11/numpy.h"
 #include "python.hpp"
 
 namespace py = pybind11;
@@ -9,7 +10,35 @@ namespace py = pybind11;
 void init_array(py::module_ &module_base) {
   py::class_<gko::array<ValueType>>(module_base, "array")
       .def(py::init<std::shared_ptr<const gko::Executor>, int>())
+      .def(py::init<std::shared_ptr<const gko::Executor>, gko::array<ValueType>&>())
+      .def(py::init(
+          [](std::shared_ptr<gko::Executor> exec,
+             py::array_t<double, py::array::c_style | py::array::forcecast> b) {
+            // for documentation of the second argument see
+            // see
+            // https://pybind11.readthedocs.io/en/stable/advanced/pycpp/numpy.html#arrays
+            /* Request a buffer descriptor from Python */
+            py::buffer_info info = b.request();
+
+            if (info.format != py::format_descriptor<ValueType>::format()) {
+              throw std::runtime_error("Incompatible dtype");
+            }
+
+            auto ref = gko::ReferenceExecutor::create();
+
+            if (info.ndim != 1) {
+              throw std::runtime_error("Only 1D arrays are supported");
+            }
+
+            auto elems = info.shape[0];
+
+            return gko::array<ValueType>(exec, (ValueType *)info.ptr,
+                                         (ValueType *)info.ptr + elems);
+          }))
       .def("fill", &gko::array<ValueType>::fill,
            "Fill the array with the given value.")
-      .def("get_size", &gko::array<ValueType>::get_num_elems);
+      .def("get_size", &gko::array<ValueType>::get_num_elems)
+      .def("at", [](const gko::array<ValueType> &arr, int idx) {
+        return arr.get_const_data()[idx];
+      });
 }
--- tests/test_array.py
+++ tests/test_array.py
@@ -21,37 +21,37 @@ def test_can_fill_array():
     executor = pyGinkgo.ReferenceExecutor()
     size = 5
     arr = pyGinkgo.base.array(executor, size)
-    arr.fill(10)
-    assert arr.at(0) == 10
+    arr.fill(10.0)
+    assert arr.at(0) == 10.0
 
 
 def test_can_instantiate_array_from_python_list():
     executor = pyGinkgo.ReferenceExecutor()
-    lst = [1, 2, 3, 4, 5]
+    lst = [1.0, 2.0, 3.0, 4.0, 5.0]
     arr = pyGinkgo.base.array(executor, lst)
     assert arr.get_size() == len(lst)
-    assert arr.at(0) == 1
-    assert arr.at(1) == 2
-    assert arr.at(2) == 3
-    assert arr.at(3) == 4
-    assert arr.at(4) == 5
+    assert arr.at(0) == 1.0
+    assert arr.at(1) == 2.0
+    assert arr.at(2) == 3.0
+    assert arr.at(3) == 4.0
+    assert arr.at(4) == 5.0
 
 
 def test_can_instantiate_array_from_numpy_array():
     executor = pyGinkgo.ReferenceExecutor()
-    np_array = np.array([1, 2, 3, 4, 5])
+    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
     arr = pyGinkgo.base.array(executor, np_array)
     assert arr.get_size() == len(np_array)
-    assert arr.at(0) == 1
-    assert arr.at(1) == 2
-    assert arr.at(2) == 3
-    assert arr.at(3) == 4
-    assert arr.at(4) == 5
+    assert arr.at(0) == 1.0
+    assert arr.at(1) == 2.0
+    assert arr.at(2) == 3.0
+    assert arr.at(3) == 4.0
+    assert arr.at(4) == 5.0
 
 
 def test_can_copy_construct_array():
     executor = pyGinkgo.ReferenceExecutor()
-    np_array = np.array([1, 2, 3, 4, 5])
+    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
     arr = pyGinkgo.base.array(executor, np_array)
     arr_copy = pyGinkgo.base.array(executor, arr)
     assert arr.get_size() == arr_copy.get_size()
