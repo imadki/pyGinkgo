e420f4158bc62d9a550b74b359357f0fc084eaa2
Gregor Olenik <gregor.olenik@web.de>
2024-10-01 10:12:36 +0200

    add some statements how to use

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -17,8 +17,17 @@ class TestSparseMatrix:
     def test_can_create_solver(self, solver_name):
         reader = getattr(pyGinkgo.matrix, "read_coo")
         fn = os.path.dirname(os.path.realpath(__file__)) + "/sparse_example.mtx"
+        # sparse = pyGinkgo.matrix.read_coo(fn, executor)
         sparse = reader(fn, self.ref)
         solver_ctr = getattr(pyGinkgo.solver, solver_name)
+        # sparse = pyGinkgo.solver.gmres(executor, sparse_matrix, iter, reset, stop)
         solver = solver_ctr(self.ref, sparse, 100, 10, 1e-06)
+
+        # to use the solver
+        # rhs and sol ginkgo.dense
+        # read rhs from disk
+        # rhs = pyGinkgo.matrix.read_dense(fn, executor)
+        # create x by copy construct and fill with zeros
+        # solver.apply(rhs, sol)
 
         assert solver == solver
