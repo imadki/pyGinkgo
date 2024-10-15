21f614c20d17302f725e0652515ef327a2d142d3
Gregor Olenik <gregor.olenik@web.de>
2024-10-15 13:55:35 +0200

    add test, fix template arguments

--- src/cpp_bindings/preconditioner/ilu.cpp
+++ src/cpp_bindings/preconditioner/ilu.cpp
@@ -4,10 +4,15 @@
 
 #include "../python.hpp"
 
-void init_ilu(py::module_ &module_solver)
+using Ilu =
+    gko::preconditioner::Ilu<gko::solver::LowerTrs<ValueType, IndexType>,
+                             gko::solver::UpperTrs<ValueType, IndexType>,
+                             false>;
+
+void init_ilu(py::module_ &module_preconditioner)
 {
-    py::class_<gko::solver::Ilu, std::shared_ptr<gko::preconditioner::Ilu>,
-               gko::LinOp>(module_solver, "Ilu")
+    py::class_<Ilu, std::shared_ptr<Ilu>, gko::LinOp>(module_preconditioner,
+                                                      "Ilu")
         .def(py::init([](std::shared_ptr<gko::Executor> exec,
                          std::shared_ptr<const gko::LinOp> system_matrix) {
             auto par_ilu_fact =
@@ -18,11 +23,7 @@ void init_ilu(py::module_ &module_solver)
 
             // Generate an ILU preconditioner factory by setting lower and upper
             // triangular solver - in this case the exact triangular solves
-            auto ilu_pre_factory =
-                gko::preconditioner::Ilu<
-                    gko::solver::LowerTrs<ValueType, IndexType>,
-                    gko::solver::UpperTrs<ValueType, IndexType>, false>::build()
-                    .on(exec);
+            auto ilu_pre_factory = Ilu::build().on(exec);
 
             // Use incomplete factors to generate ILU preconditioner
             return gko::share(ilu_pre_factory->generate(par_ilu));
