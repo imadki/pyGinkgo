9eabd802c63d13417d61f2ae9a1c54e914b1bcdd
Gregor Olenik <gregor.olenik@web.de>
2024-10-15 13:56:52 +0200

    update solver test

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -4,7 +4,7 @@
 
 import os
 import sys
-import pyGinkgo
+import pyGinkgoBindings as pgb
 import pytest
 
 sys.path.append("../build")
@@ -12,15 +12,15 @@ sys.path.append("../build")
 
 @pytest.mark.parametrize("solver", ["gmres"])
 class TestSparseMatrix:
-    ref = pyGinkgo.ReferenceExecutor()
+    ref = pgb.ReferenceExecutor()
 
     def test_can_create_solver(self, solver_name):
-        reader = getattr(pyGinkgo.matrix, "read_coo")
+        reader = getattr(pgb.matrix, "read_coo")
         fn = os.path.dirname(os.path.realpath(__file__)) + "/sparse_example.mtx"
-        # sparse = pyGinkgo.matrix.read_coo(fn, executor)
+        # sparse = pgb.matrix.read_coo(fn, executor)
         sparse = reader(fn, self.ref)
-        solver_ctr = getattr(pyGinkgo.solver, solver_name)
-        # sparse = pyGinkgo.solver.gmres(executor, sparse_matrix, iter, reset, stop)
+        solver_ctr = getattr(pgb.solver, solver_name)
+        # sparse = pgb.solver.gmres(executor, sparse_matrix, iter, reset, stop)
         solver = solver_ctr(self.ref, sparse, 100, 10, 1e-06)
 
         # to use the solver
