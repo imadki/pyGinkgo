6dd7801d32cc4bb4db862f75b3c4753bbc42d112
Gregor Olenik <gregor.olenik@web.de>
2024-11-05 15:59:52 +0100

    fixup! broken include after move

--- src/cpp_bindings/solver/config_solver.cpp
+++ src/cpp_bindings/solver/config_solver.cpp
@@ -5,7 +5,7 @@
 #include <pybind11/pybind11.h>
 #include "pybind11/numpy.h"
 
-#include "python.hpp"
+#include "../python.hpp"
 
 #include <ginkgo/extensions/config/json_config.hpp>
 #include "ginkgo/ginkgo.hpp"
