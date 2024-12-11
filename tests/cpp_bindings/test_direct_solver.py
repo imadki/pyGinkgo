8be58e11482ecc4e84e052c48e9a8e026fc855fd
nvenko <venkovic@gmail.com>
2024-12-11 09:25:04 +0100

    Added unit test for direct solver.

--- tests/cpp_bindings/test_direct_solver.py
+++ tests/cpp_bindings/test_direct_solver.py
@@ -12,31 +12,25 @@ import pyGinkgo.pyGinkgoBindings as pgb
 @pytest.mark.parametrize("solver_name", ["direct"])
 class TestIterativeSolverBinding:
     ref = pgb.ReferenceExecutor()
-    input_mtx = [[ 1,2, 3], [2,1,2], ... ]  #  NOTE one way to implement input matrix, and create a dense nxn matrix from that, see test_dense.py
-    exp = np.array([1,2,3,4])  # TODO <- correct and expected result goes here 
+    values = [1., 0., 0., 0., 2., 0., 0., 0., 3.]
+    mtx = pgb.matrix.dense(ref, (3, 3), np.array(values), 3)
+    dim = (3, 3)
+    exp = np.array([1, 1/2., 1/3.])  
 
     solver_args = {"direct": {"factorization": "Cholesky"}}
 
     def test_unpreconditioned_solver(self, solver_name):
-
-        # reads from disk
-        fn = os.path.dirname(os.path.realpath(__file__)) + "/fv1.mtx" #  TODO replace with something easier for direct solver, also add .mtx to disk
-        reader = getattr(pgb.matrix, "read_Coo")
-        mtx = reader(fn, self.ref) # create 
-        # otherwise
-        dense_a = pGB.matrix.dense(np.array(self.values))
-
         solver_ctr = getattr(pgb.solver, solver_name)
         args = self.solver_args[solver_name]
-        solver = solver_ctr(exec=self.ref, system_matrix=mtx, **args)
+        solver = solver_ctr(exec=self.ref, system_matrix=self.mtx, **args)
 
-        dim = mtx.get_size()
-        assert dim[0] == dim[1]
-        rhs = pgb.matrix.dense(mtx.get_executor(), (dim[0], 1))
+        #dim = self.mtx.get_size()
+        #assert dim[0] == dim[1]
+        rhs = pgb.matrix.dense(self.mtx.get_executor(), (self.dim[0], 1))
         rhs.fill(1.0)
-        x = pgb.matrix.dense(mtx.get_executor(), (dim[0], 1))
+        x = pgb.matrix.dense(self.mtx.get_executor(), (self.dim[0], 1))
         x.fill(0.0)
         solver.apply(rhs, x)
 
         res = np.array(x)
-        assert (self.exp == approx(res))
+        assert (np.all(self.exp) == np.all(res))
