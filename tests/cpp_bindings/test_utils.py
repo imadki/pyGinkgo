586fbc4f86285fe593448f70a1d1c4afddcaa466
Roman Mishchuk <romamiykg@gmail.com>
2025-02-15 22:07:58 +0200

    Dense tests passing

--- tests/cpp_bindings/test_utils.py
+++ tests/cpp_bindings/test_utils.py
@@ -5,10 +5,8 @@
 import numpy as np
 from typing import Union
 
-import pyGinkgo.pyGinkgoBindings as pGB
 
-
-def verify_dense_vec(mtx: pGB.matrix.dense, values: Union[list, np.ndarray]):
+def verify_dense_vec(mtx, values: Union[list, np.ndarray]):
     """ """
     assert mtx.get_num_stored_elements() == len(values)
     for i in range(len(values)):
@@ -17,3 +15,7 @@ def verify_dense_vec(mtx: pGB.matrix.dense, values: Union[list, np.ndarray]):
     assert mtx.at(2, 0) == mtx.at(2)
     # test if it can be called several times
     assert mtx.at(2, 0) == mtx.at(2)
+
+
+def verify_within_precision(a, b, precision=1e-3):
+    assert abs(a - b) <= precision
