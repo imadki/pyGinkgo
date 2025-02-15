8148ee8c8db5288d8111d22891d60eb07582c4d0
Roman Mishchuk <romamiykg@gmail.com>
2025-02-15 20:20:06 +0200

    Array test passes everything apart from half

--- tests/cpp_bindings/test_array.py
+++ tests/cpp_bindings/test_array.py
@@ -8,14 +8,20 @@ import numpy as np
 
 import pyGinkgo.pyGinkgoBindings as pGB
 
+d_type_map = {
+    "int32": np.int32,
+    "int64": np.longlong,
+    "half": np.float16,
+    "float": np.float32,
+    "double": np.float64,
+}
 
-@pytest.mark.parametrize("data_type", ["float", "double", "int"])
+
+@pytest.mark.parametrize("data_type", list(d_type_map.keys()))
 class TestArray:
     ref = pGB.ReferenceExecutor()
     size = 5
     lst = [1.0, 2.0, 3.0, 4.0, 5.0]
-
-    d_type_map = {"int": int, "float": float, "double": np.double}
 
     def test_init_empty_array(self, data_type):
         ctr = getattr(pGB.base, "array_" + data_type)
@@ -25,22 +31,22 @@ class TestArray:
     def test_can_fill_array(self, data_type):
         ctr = getattr(pGB.base, "array_" + data_type)
         arr = ctr(self.ref, self.size)
-        fill_value = self.d_type_map[data_type](10)
+        fill_value = d_type_map[data_type](10)
         arr.fill(fill_value)
         assert arr.at(0) == fill_value
 
     def test_can_instantiate_array_from_numpy_array(self, data_type):
-        np_array = np.array(self.lst, dtype=self.d_type_map[data_type])
+        np_array = np.array(self.lst, dtype=d_type_map[data_type])
         ctr = getattr(pGB.base, "array_" + data_type)
         arr = ctr(self.ref, np_array)
         assert arr.get_size() == len(np_array)
         for i in range(self.size):
-            expect = self.d_type_map[data_type](self.lst[i])
+            expect = d_type_map[data_type](self.lst[i])
             assert arr.at(i) == expect
 
     def test_can_copy_construct_array(self, data_type):
         ctr = getattr(pGB.base, "array_" + data_type)
-        np_array = np.array(self.lst)
+        np_array = np.array(self.lst, dtype=d_type_map[data_type])
         arr = ctr(self.ref, np_array)
         arr_copy = ctr(self.ref, arr)
 
@@ -48,7 +54,7 @@ class TestArray:
 
     def test_can_construct_np_array_from_gko_array(self, data_type):
         ctr = getattr(pGB.base, "array_" + data_type)
-        np_array = np.array(self.lst)
+        np_array = np.array(self.lst, dtype=d_type_map[data_type])
         arr = ctr(self.ref, np_array)
         np_array_copy = np.array(arr)
 
@@ -56,8 +62,8 @@ class TestArray:
 
     def test_can_reduce_add_array(self, data_type):
         ctr = getattr(pGB.base, "array_" + data_type)
-        np_array = np.array(self.lst, dtype=self.d_type_map[data_type])
+        np_array = np.array(self.lst, dtype=d_type_map[data_type])
         arr = ctr(self.ref, np_array)
-        init_value = self.d_type_map[data_type](0.0)
-        expected_value = self.d_type_map[data_type](15.0)
+        init_value = d_type_map[data_type](0.0)
+        expected_value = d_type_map[data_type](15.0)
         assert pGB.base.reduce_add(arr, init_value) == expected_value
