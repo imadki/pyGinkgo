1d71f77a7a68dd3dab2759454739ec452f6113cc
Roman Mishchuk <romamiykg@gmail.com>
2025-02-15 22:41:34 +0200

    Not fully passing torch test

--- tests/cpp_bindings/test_torch.py
+++ tests/cpp_bindings/test_torch.py
@@ -14,35 +14,49 @@ except ImportError:
 import pyGinkgo.pyGinkgoBindings as pgb
 import pytest
 
+d_type_map = {
+    "half": np.float16,
+    "float": np.float32,
+    "double": np.float64,
+}
 
-@pytest.mark.skipif(not torch_avail, reason="requires pytorch")
-def test_can_create_array_from_torch():
-    executor = pgb.ReferenceExecutor()
-    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    torch_array = torch.asarray(np_array)
-    arr = pgb.base.array_float(executor, torch_array)
-    arr_copy = pgb.base.array_float(executor, arr)
-    assert arr.get_size() == arr_copy.get_size()
-    assert pgb.base.reduce_add(arr, 0.0) == 15.0
-
-
-@pytest.mark.skipif(not torch_avail, reason="requires pytorch")
-def test_can_create_torch_array_from_gko_array():
-    executor = pgb.ReferenceExecutor()
-    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    arr = pgb.base.array_float(executor, np_array)
-    torch_array = torch.asarray(arr)
-    assert torch_array.size(dim=0) == np_array.size
+torch_d_type_map = {
+    "half": torch.float16,
+    "float": torch.float32,
+    "double": torch.float64,
+}
 
 
 @pytest.mark.skipif(not torch_avail, reason="requires pytorch")
-def test_can_create_dense_from_torch_tensor():
-    executor = pgb.ReferenceExecutor()
-    data = [[1.0, 2.0], [3.0, 4.0]]
-    torch_tensor = torch.tensor(data, dtype=float)
-    dense = pgb.matrix.dense(executor, torch_tensor.__array__())
-    assert dense.get_num_stored_elements() == 4
-    assert dense.at(0, 1) == 2.0
-    assert dense.at(1, 1) == 4.0
-    assert dense.get_size()[0] == 2
-    assert dense.get_size()[1] == 2
+@pytest.mark.parametrize("data_type", list(d_type_map.keys()))
+class TestTorchInteroperability:
+    def test_can_create_array_from_torch(self, data_type):
+        executor = pgb.ReferenceExecutor()
+        ctr = getattr(pgb.base, "array_" + data_type)
+        np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0], dtype=d_type_map[data_type])
+        torch_array = torch.asarray(np_array)
+        arr = ctr(executor, torch_array)
+        arr_copy = ctr(executor, arr)
+        assert arr.get_size() == arr_copy.get_size()
+        assert pgb.base.reduce_add(arr, 0.0) == 15.0
+
+    def test_can_create_torch_array_from_gko_array(self, data_type):
+        executor = pgb.ReferenceExecutor()
+        ctr = getattr(pgb.base, "array_" + data_type)
+        np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0], dtype=d_type_map[data_type])
+        arr = ctr(executor, np_array)
+        torch_array = torch.asarray(arr)
+        assert torch_array.size(dim=0) == np_array.size
+
+    def test_can_create_dense_from_torch_tensor(self, data_type):
+        executor = pgb.ReferenceExecutor()
+        ctr = getattr(pgb.matrix, "dense_" + data_type)
+        data = [[1.0, 2.0], [3.0, 4.0]]
+        # TODO: the created tensor is broken for data_type = "half" and "double"
+        torch_tensor = torch.tensor(data, dtype=torch_d_type_map[data_type])
+        dense = ctr(executor, torch_tensor.__array__())
+        assert dense.get_num_stored_elements() == 4
+        assert dense.at(0, 1) == 2.0
+        assert dense.at(1, 1) == 4.0
+        assert dense.get_size()[0] == 2
+        assert dense.get_size()[1] == 2
