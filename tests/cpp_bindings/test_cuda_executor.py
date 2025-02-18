905a1e09f4a55db6765d66eed9ec136dc320a093
Roman Mishchuk <romamiykg@gmail.com>
2025-02-19 01:29:02 +0200

    Cuda parametrized testing

--- tests/cpp_bindings/test_cuda_executor.py
+++ tests/cpp_bindings/test_cuda_executor.py
@@ -9,50 +9,57 @@ import pyGinkgo.pyGinkgoBindings as pGB
 
 from test_utils import verify_dense_vec
 
-
-def test_array_cuda():
-    if pGB.CudaExecutor.get_num_devices() < 1:
-        pytest.skip("CUDA is not available")
-
-    executor = pGB.CudaExecutor()
-    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    arr = pGB.base.array_double(executor, np_array)
-    arr_copy = pGB.base.array_double(executor, arr)
-    assert arr.get_size() == arr_copy.get_size()
-    assert pGB.base.reduce_add(arr, 0.0) == 15.0
+d_type_map = {
+    "half": np.float16,
+    "float": np.float32,
+    "double": np.float64,
+}
 
 
-def test_dense_copy_to_host():
-    if pGB.CudaExecutor.get_num_devices() < 1:
-        pytest.skip("CUDA is not available")
+@pytest.mark.parametrize("data_type", list(d_type_map.keys()))
+class TestCuda:
+    def test_array_cuda(self, data_type):
+        if pGB.CudaExecutor.get_num_devices() < 1:
+            pytest.skip("CUDA is not available")
 
-    master = pGB.ReferenceExecutor()
-    executor = pGB.CudaExecutor(master=master)
-    np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
-    dense = pGB.matrix.dense_double(executor, np_array)
-    dense_on_master = dense.copy_to_host()
-    assert dense.get_executor() == executor
-    assert dense_on_master.get_executor() == master
-    assert id(dense) != id(dense_on_master)
-    verify_dense_vec(dense_on_master, np_array)
+        executor = pGB.CudaExecutor()
+        np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0], dtype=d_type_map[data_type])
+        ctr = getattr(pGB.base, "array_" + data_type)
+        arr = ctr(executor, np_array)
+        arr_copy = ctr(executor, arr)
+        assert arr.get_size() == arr_copy.get_size()
+        assert pGB.base.reduce_add(arr, 0.0) == 15.0
 
+    def test_dense_copy_to_host(self, data_type):
+        if pGB.CudaExecutor.get_num_devices() < 1:
+            pytest.skip("CUDA is not available")
 
-def test_device_id():
-    if pGB.CudaExecutor.get_num_devices() < 1:
-        pytest.skip("CUDA is not available")
+        master = pGB.ReferenceExecutor()
+        executor = pGB.CudaExecutor(master=master)
+        np_array = np.array([1.0, 2.0, 3.0, 4.0, 5.0], dtype=d_type_map[data_type])
+        ctr = getattr(pGB.matrix, "dense_" + data_type)
+        dense = ctr(executor, np_array)
+        dense_on_master = dense.copy_to_host()
+        assert dense.get_executor() == executor
+        assert dense_on_master.get_executor() == master
+        assert id(dense) != id(dense_on_master)
+        verify_dense_vec(dense_on_master, np_array)
 
-    executor = pGB.CudaExecutor(0)
-    assert executor.device_id == 0
-    with pytest.raises(AttributeError):
-        executor.device_id = 1
+    def test_device_id(self, data_type):
+        if pGB.CudaExecutor.get_num_devices() < 1:
+            pytest.skip("CUDA is not available")
 
+        executor = pGB.CudaExecutor(0)
+        assert executor.device_id == 0
+        with pytest.raises(AttributeError):
+            executor.device_id = 1
 
-def test_master():
-    if pGB.CudaExecutor.get_num_devices() < 1:
-        pytest.skip("CUDA is not available")
+    def test_master(self, data_type):
+        if pGB.CudaExecutor.get_num_devices() < 1:
+            pytest.skip("CUDA is not available")
 
-    master = pGB.ReferenceExecutor()
-    executor = pGB.CudaExecutor(0, master)
-    assert id(executor.master) == id(master)
-    with pytest.raises(AttributeError):
-        executor.master = pGB.ReferenceExecutor()
+        master = pGB.ReferenceExecutor()
+        executor = pGB.CudaExecutor(0, master)
+        assert id(executor.master) == id(master)
+        with pytest.raises(AttributeError):
+            executor.master = pGB.ReferenceExecutor()
