895c7d5e40fd90f1a7bb5c117e003dd5710dc308
Roman Mishchuk <romamiykg@gmail.com>
2025-06-09 23:01:19 +0300

    Comments on fixing dart issue

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,4 +2,13 @@
 #
 # SPDX-FileCopyrightText: 2025 pyGinkgo authors
 
-# https://cmake.org/pipermail/cmake/2015-January/059742.html
+# This file added as per:
+# https://cmake.org/pipermail/cmake/2015-January/059742.html : >> Do you have a
+# CTestConfig.cmake file in your source tree? >> >> If you do, then ctest will
+# load that instead of looking for the >> DartConfiguration.tcl file in your
+# build tree... Even if it's empty >> because you don't submit to a CDash
+# server, the presence of >> CTestConfig.cmake in your top level source tree
+# should suppress this >> error message.
+
+# In such a way to suppress "Cannot find file:
+# /sourcerepo/build/DartConfiguration.tcl"
